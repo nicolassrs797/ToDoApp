1cc19930be4d040b57c7071f2301e098105d8966
Nicolas Silva <nicsrs@nsrs.work>
2021-07-21 08:25:53 -0300

    Alteracoes do desafio principal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,38 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
+import { ItemWrapper } from '../components/ItemWrapper';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
+
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    let data ={
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+    setTasks(oldTasks => [...oldTasks, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    let updateTasks = tasks.map(tasks => ({...tasks}));
+    let taskOnChange = updateTasks.find(task => task.id === id);
+    if(!taskOnChange){
+      return;
+    } else {
+      taskOnChange.done = !taskOnChange.done;
+    }
+    setTasks(updateTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id != id
+    ));
   }
 
   return (
